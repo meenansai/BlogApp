c08985d564517acbefa0be1a22c0947c14ec704c
meenansai <v.saimeenan@gmail.com>
2020-07-10 13:36:19 +0530

    added home screen init files

--- app/src/main/java/com/example/blogapp/MainActivity.java
+++ app/src/main/java/com/example/blogapp/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.blogapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loginBtn = (Button) findViewById(R.id.main_activity_loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: Login Button Clicked");
+                Intent intent = new Intent(MainActivity.this,HomeScreenActivity.class);
+                startActivity(intent);
+                
+            }
+        });
     }
 }
