a02fbc86abc516d1b6a8f0233301ef5ed5298b7c
Rakesh Sirikonda <sirikondarakesh001@gmail.com>
2020-07-10 21:41:51 +0530

    firebase linked

--- app/src/main/java/com/example/blogapp/Activity/HomeScreenActivity.java
+++ app/src/main/java/com/example/blogapp/Activity/HomeScreenActivity.java
@@ -2,37 +2,23 @@ package com.example.blogapp.Activity;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
+
 import android.view.MenuItem;
-import android.view.View;
+
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.Volley;
-import com.example.blogapp.Adapter.ArticleRecyclerView;
 import com.example.blogapp.Fragment.FavoritesFragment;
 import com.example.blogapp.Fragment.HomeFragment;
-import com.example.blogapp.Model.Article;
+
+import com.example.blogapp.Fragment.ProfileFragment;
 import com.example.blogapp.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class HomeScreenActivity extends AppCompatActivity {
     private static final String TAG = "HomeScreenActivity";
@@ -40,6 +26,7 @@ public class HomeScreenActivity extends AppCompatActivity {
     // Initializing all Fragments
     final Fragment fragment1 = new HomeFragment();
     final Fragment fragment2 = new FavoritesFragment();
+    final Fragment fragment3 = new ProfileFragment();
 
     final FragmentManager fm = getSupportFragmentManager();
     Fragment active = fragment1;
@@ -55,6 +42,7 @@ public class HomeScreenActivity extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
+        fm.beginTransaction().add(R.id.mainFrameLayout, fragment3, "3").hide(fragment3).commit();
         fm.beginTransaction().add(R.id.mainFrameLayout, fragment2, "2").hide(fragment2).commit();
         fm.beginTransaction().add(R.id.mainFrameLayout, fragment1, "1").commit();
     }
@@ -67,14 +55,17 @@ public class HomeScreenActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.nav_articles:
                     fm.beginTransaction().hide(active).show(fragment1).commit();
-//                    getSupportActionBar().setTitle("Vineyard Ministries");
                     active = fragment1;
                     return true;
 
                 case R.id.nav_favorites:
                     fm.beginTransaction().hide(active).show(fragment2).commit();
-//                    getSupportActionBar().setTitle("Songs");
                     active = fragment2;
+                    return true;
+
+                case R.id.nav_profile:
+                    fm.beginTransaction().hide(active).show(fragment3).commit();
+                    active = fragment3;
                     return true;
             }
             return false;
---
+++ app/src/main/java/com/example/blogapp/Fragment/ProfileFragment.java
@@ -0,0 +1,40 @@
+package com.example.blogapp.Fragment;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.blogapp.R;
+
+
+public class ProfileFragment extends Fragment {
+    private static final String TAG = "ProfileFragment";
+
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        return view;
+    }
+}
