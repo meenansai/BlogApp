2bb2485043fc7cfae81c125030917d4a1b153e72
meenansai <v.saimeenan@gmail.com>
2020-07-10 20:32:54 +0530

    Article details page added and list articles ui changed

--- app/src/main/java/com/example/blogapp/Activity/ArticleDetailPage.java
+++ app/src/main/java/com/example/blogapp/Activity/ArticleDetailPage.java
@@ -2,22 +2,57 @@ package com.example.blogapp.Activity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.blogapp.Model.Article;
 import com.example.blogapp.R;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 public class ArticleDetailPage extends AppCompatActivity {
     private static final String TAG = "ArticleDetailPage";
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article_detail_page);
-
+        Article article = new Article();
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             Log.d(TAG, "onCreate: Title:" +extras.getString("title"));
+            article.setTitle(extras.getString("title"));
+            article.setSubTitle(extras.getString("subtitle"));
+            article.setArticleData(extras.getString("article_data"));
+            article.setAuthor(extras.getString("author"));
+            article.setImgUrl(extras.getString("img"));
+            article.setVideoUrl(extras.getString("videoUrl"));
         }
+
+        //Image
+        ImageView imgView = (ImageView) findViewById(R.id.articleDetailImageView);
+        Glide.with(this).load(article.getImgUrl()).apply(new RequestOptions().centerCrop()).into(imgView);
+
+        //Title
+        TextView titletv = (TextView) findViewById(R.id.titleTextViewValue);
+        titletv.setText(article.getTitle());
+
+        //subtitle
+        TextView subtitletv = (TextView) findViewById(R.id.subtitileTextViewValue);
+        subtitletv.setText(article.getSubTitle());
+
+        //author
+        TextView authortv = (TextView) findViewById(R.id.authorTextViewValue);
+        authortv.setText(article.getAuthor());
+
+        //description
+        TextView description = (TextView) findViewById(R.id.descriptionTextViewValue);
+        description.setText(article.getArticleData());
+
+        //subtitle
+        TextView videoUrl = (TextView) findViewById(R.id.videoUrlTextViewValue);
+        videoUrl.setText(article.getVideoUrl());
     }
 }
